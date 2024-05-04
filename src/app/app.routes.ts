7afa03e3f948f1e67a668a0b2b08996cb4307b11
Anna Brito <annet.rj@gmail.com>
2024-05-04 20:40:16 -0300

    Add book list route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { authenticatedGuard } from './guards/authenticated.guard';
 
 export const routes: Routes = [
     { path: 'login', component: SignInComponent },
+    { path: 'livros', component: BookListComponent, canActivate: [authenticatedGuard] },
     { path: '**', redirectTo: 'login' }
 ];
