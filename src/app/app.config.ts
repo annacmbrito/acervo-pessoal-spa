b5b6aac6a6491dc9589997082c8e1e29b56f36fb
Anna Brito <annet.rj@gmail.com>
2024-05-05 11:16:03 -0300

    Setup ngx-toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideToastr(),
+    provideAnimations(),
     importProvidersFrom(HttpClientModule),
   ]
 };
