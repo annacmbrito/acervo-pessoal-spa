14a1a704c7e7a84c0cfcf1af2a8ec1ee79323329
Anna Brito <annet.rj@gmail.com>
2024-05-07 20:44:58 -0300

    Add route to save new book

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { BookListComponent } from './pages/book-list/book-list.component';
 import { authenticatedGuard } from './guards/authenticated.guard';
+import { BookFormComponent } from './pages/book-form/book-form.component';
 
 export const routes: Routes = [
     { path: 'login', component: SignInComponent },
     { path: 'livros', component: BookListComponent, canActivate: [authenticatedGuard] },
+    { path: 'livros/novo', component: BookFormComponent, canActivate: [authenticatedGuard] },
     { path: '**', redirectTo: 'login' }
 ];
