b54d7d314e44929cf65b05788e2464f9e5f5256e
Anna Brito <annet.rj@gmail.com>
2024-05-07 23:38:14 -0300

    Add new book button in book list

--- src/app/pages/book-list/book-list.component.ts
+++ src/app/pages/book-list/book-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterLink],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
