fc4cda78e5310f1c3f096b58350639409ae3ada1
Anna Brito <annet.rj@gmail.com>
2024-05-07 23:24:59 -0300

    Add edit mode control in book form

--- src/app/pages/book-form/book-form.component.ts
+++ src/app/pages/book-form/book-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-form',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './book-form.component.html',
   styleUrl: './book-form.component.scss'
 })
-export class BookFormComponent {
+export class BookFormComponent implements OnInit {
 
+  public bookId?: number;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      if(params.has("id")) {
+        this.bookId = parseInt(params.get('id')!, 10);
+      }
+    })
+  }
+
+  public isEditMode(): boolean {
+    return !!this.bookId;
+  }
 }
