f62fe4da7a3971bcb575ee491a5659410fd86b18
Anna Brito <annet.rj@gmail.com>
2024-05-14 00:52:46 -0300

    Add method to find all subcategories

--- src/app/services/subcategory.service.ts
+++ src/app/services/subcategory.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Page } from '../models/page.model';
+import { Subcategory } from '../models/subcategory.model';
+import { convertPageToParam, convertParamsToPage } from '../util/mappers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubcategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAll(page: Page<Subcategory>): Observable<Page<Subcategory>> {
+    return this.http.get<any>(`${environment.apiBaseUrl}/api/v1/subcategories/`, {
+      params: convertPageToParam(page),
+    }).pipe(map(data => convertParamsToPage<Subcategory>(data)));
+  }
 }
