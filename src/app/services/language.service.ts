bc1733dcb6ee1096d44ffe8b9a13f27d91389ca1
Anna Brito <annet.rj@gmail.com>
2024-05-13 22:43:00 -0300

    Add method to get all languages

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Page } from '../models/page.model';
+import { Language } from '../models/language.model';
+import { Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { convertPageToParam, convertParamsToPage } from '../util/mappers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAll(page: Page<Language>): Observable<Page<Language>> {
+    return this.http.get<any>(`${environment.apiBaseUrl}/api/v1/languages/`, {
+      params: convertPageToParam(page),
+    }).pipe(map(data => convertParamsToPage<Language>(data)));
+  }
 }
