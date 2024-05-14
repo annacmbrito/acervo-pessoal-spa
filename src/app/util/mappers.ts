492da756016d18224dc601313b955333f47c1efa
Anna Brito <annet.rj@gmail.com>
2024-05-13 22:40:08 -0300

    Add page mappers

---
+++ src/app/util/mappers.ts
@@ -0,0 +1,29 @@
+import { Page } from "../models/page.model";
+
+export function convertParamsToPage<T>(data: any): Page<T> {
+    return {
+        size: data.size,
+        offset: data.offset,
+        orderBy: data.order_by,
+        orderDirection: data.order_direction,
+        numberOfElements: data.number_of_elements,
+        content: data.content
+    };
+}
+
+export function convertPageToParam(page: Page<any>): any {
+    let param = {} as any;
+    if (page.size) {
+        param.size = page.size;
+    }
+    if (page.offset) {
+        param.offset = page.offset;
+    }
+    if (page.orderBy) {
+        param.order_by = page.orderBy;
+    }
+    if (page.orderDirection) {
+        param.order_direction = page.orderDirection;
+    }
+    return param;
+}
