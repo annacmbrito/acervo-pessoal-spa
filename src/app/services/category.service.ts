0522ed3cd4cf61055b1ddd57857415db8ea3e500
Anna Brito <annet.rj@gmail.com>
2024-05-14 00:45:34 -0300

    Add method to find all categories

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Page } from '../models/page.model';
+import { Author } from '../models/author.model';
+import { environment } from '../../environments/environment';
+import { Observable, map } from 'rxjs';
+import { convertPageToParam, convertParamsToPage } from '../util/mappers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAll(page: Page<Author>): Observable<Page<Author>> {
+    return this.http.get<any>(`${environment.apiBaseUrl}/api/v1/categories/`, {
+      params: convertPageToParam(page),
+    }).pipe(map(data => convertParamsToPage<Author>(data)));
+  }
 }
