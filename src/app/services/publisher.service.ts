3546256586b9188421e4ae55caae1e0c0abc782f
Anna Brito <annet.rj@gmail.com>
2024-05-14 01:13:10 -0300

    Add method to get all publishers

--- src/app/services/publisher.service.ts
+++ src/app/services/publisher.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Page } from '../models/page.model';
+import { Publisher } from '../models/publisher.model';
+import { convertPageToParam, convertParamsToPage } from '../util/mappers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublisherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAll(page: Page<Publisher>): Observable<Page<Publisher>> {
+    return this.http.get<any>(`${environment.apiBaseUrl}/api/v1/publishers/`, {
+      params: convertPageToParam(page),
+    }).pipe(map(data => convertParamsToPage<Publisher>(data)));
+  }
 }
