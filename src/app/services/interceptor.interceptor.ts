2b0ad21e68597d82558e0de2da1de12844c6c722
Anna Brito <annet.rj@gmail.com>
2024-05-13 23:14:55 -0300

    Add interceptor to add token jwt in every request

---
+++ src/app/services/interceptor.interceptor.ts
@@ -0,0 +1,27 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const router = inject(Router);
+  const toastr = inject(ToastrService);
+  const authService = inject(AuthService);
+
+  const token = authService.getToken();
+  
+  return next(req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  })).pipe(catchError(error => {
+    if (error.status === 401) {
+      authService.signOut();
+      router.navigate(['/login']);
+      toastr.error('Sessão expirada');
+    }
+    return throwError(() => new Error());
+  }));
+};
