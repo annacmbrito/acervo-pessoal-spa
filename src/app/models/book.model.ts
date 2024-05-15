9e29c1900870614d93ad058cac34aab0faa6b4a4
Anna Brito <annet.rj@gmail.com>
2024-05-14 23:18:58 -0300

    Add method to save book

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,17 @@
+export interface Book {
+    
+}
+
+export interface BookDTO {
+    name: string
+    description: string
+    comment: string
+    pages: number
+    rating: number
+    status: string
+    author: string,
+    language: string,
+    publisher: string,
+    category: string,
+    subcategory: string,
+}
--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { BookDTO } from '../models/book.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public save(book: BookDTO): Observable<any> {
+    return this.http.post<any>(`${environment.apiBaseUrl}/api/v1/books/`, book);
+  }
 }
