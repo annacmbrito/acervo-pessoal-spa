3150ab561f9cc4439d897b63142702622d4f3085
Anna Brito <annet.rj@gmail.com>
2024-05-16 07:40:30 -0300

    Add method to get all books

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { BookDTO } from '../models/book.model';
+import { Book, BookDTO } from '../models/book.model';
+import { Page } from '../models/page.model';
+import { convertPageToParam, convertParamsToPage } from '../util/mappers';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +13,12 @@ import { BookDTO } from '../models/book.model';
 export class BookService {
 
   constructor(private http: HttpClient) { }
+
+  public getAll(page: Page<Book>): Observable<Page<Book>> {
+    return this.http.get<any>(`${environment.apiBaseUrl}/api/v1/books/`, {
+      params: convertPageToParam(page),
+    }).pipe(map(data => convertParamsToPage<Book>(data)));
+  }
 
   public save(book: BookDTO): Observable<any> {
     return this.http.post<any>(`${environment.apiBaseUrl}/api/v1/books/`, book);
