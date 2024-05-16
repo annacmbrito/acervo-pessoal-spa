fcc5734c7089e1de0b152a2ed39a772fa831cccc
Anna Brito <annet.rj@gmail.com>
2024-05-16 20:54:00 -0300

    Display logged user name in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +10,21 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
-  @Input()
   public name: string = '';
   
   constructor(
     private router: Router,
     private authService: AuthService,
+    private userService: UserService,
   ) {}
+
+  ngOnInit(): void {
+    this.userService.getLoggedUserData().subscribe({
+      next: user => this.name = `${user.first_name} ${user.last_name}`,
+    });
+  }
 
   public signOut(): void {
     this.authService.signOut();
