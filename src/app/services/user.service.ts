94fee3afcc905e92009808a0216c33aeafdbe760
Anna Brito <annet.rj@gmail.com>
2024-05-16 20:52:26 -0300

    Add route to get logged user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getLoggedUserData(): Observable<User> {
+    return this.http.get<User>(`${environment.apiBaseUrl}/api/v1/users/me`);
+  }
 }
