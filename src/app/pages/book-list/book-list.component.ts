6ac442bd82964b2de9a89096f2cb7387d75862af
Anna Brito <annet.rj@gmail.com>
2024-05-16 20:49:19 -0300

    Show book list when sign in

--- src/app/pages/book-list/book-list.component.ts
+++ src/app/pages/book-list/book-list.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { RouterLink } from '@angular/router';
+import { BookService } from '../../services/book.service';
+import { BookDetailsComponent } from '../../components/book-details/book-details.component';
+import { Page } from '../../models/page.model';
+import { Book } from '../../models/book.model';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [NavbarComponent, RouterLink],
+  imports: [NavbarComponent, RouterLink, BookDetailsComponent],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  
+  public bookPage: Page<Book> = { 
+    orderBy: 'name', 
+    content: [] 
+  };
 
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.bookService.getAll(this.bookPage).subscribe({
+      next: page => this.bookPage = page
+    })
+  }
 }
