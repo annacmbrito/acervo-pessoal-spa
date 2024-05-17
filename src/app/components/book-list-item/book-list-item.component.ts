ca366bfe7ff7a80bb1b85a082a8aaa661366d1b1
Anna Brito <annet.rj@gmail.com>
2024-05-16 21:02:23 -0300

    Add component book list item

---
+++ src/app/components/book-list-item/book-list-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Book } from '../../models/book.model';
+import { RatingComponent } from '../rating/rating.component';
+import { BookStatus } from '../../models/book-status.enum';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-book-list-item',
+  standalone: true,
+  imports: [RatingComponent, RouterLink],
+  templateUrl: './book-list-item.component.html',
+  styleUrl: './book-list-item.component.scss'
+})
+export class BookListItemComponent {
+  @Input()
+  public book!: Book;
+
+  public getBookStatus() {
+    const statusKey = this.book.status as keyof typeof BookStatus;
+    return BookStatus[statusKey];
+  }
+}
