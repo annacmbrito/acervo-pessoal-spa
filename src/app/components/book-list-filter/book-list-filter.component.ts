b5ea28fd37baaa1580b351f657a3210fbc2ac8c3
Anna Brito <annet.rj@gmail.com>
2024-05-16 22:23:44 -0300

    Add component to represent book list filter

---
+++ src/app/components/book-list-filter/book-list-filter.component.ts
@@ -0,0 +1,74 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { BookFilterDTO } from '../../models/book-filter.dto';
+import { Category } from '../../models/category.model';
+import { Page } from '../../models/page.model';
+import { Publisher } from '../../models/publisher.model';
+import { Subcategory } from '../../models/subcategory.model';
+import { CategoryService } from '../../services/category.service';
+import { PublisherService } from '../../services/publisher.service';
+import { SubcategoryService } from '../../services/subcategory.service';
+import { RatingComponent } from '../rating/rating.component';
+
+@Component({
+  selector: 'app-book-list-filter',
+  standalone: true,
+  imports: [ReactiveFormsModule, RatingComponent],
+  templateUrl: './book-list-filter.component.html',
+  styleUrl: './book-list-filter.component.scss'
+})
+export class BookListFilterComponent implements OnInit {
+
+  @Output()
+  public filter: EventEmitter<BookFilterDTO> = new EventEmitter();
+  
+  public form: FormGroup = this.formBuilder.group({
+    token: [''],
+    rating: [''],
+    publisher_id: [''],
+    category_id: [''],
+    subcategory_id: [''],
+    orderBy: [''],
+  });
+
+  public publisherPage: Page<Publisher> = { 
+    orderBy: 'name', 
+    content: [] 
+  };
+  public categoryPage: Page<Category> = { 
+    orderBy: 'name', 
+    content: [] 
+  };
+  public subcategoryPage: Page<Subcategory> = { 
+    orderBy: 'name', 
+    content: [] 
+  };
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private publisherService: PublisherService,
+    private categoryService: CategoryService,
+    private subcategoryService: SubcategoryService,
+  ) {}
+  
+  ngOnInit(): void {
+    this.form.get('token')?.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+    ).subscribe(() => this.emitFilter());
+    this.publisherService.getAll(this.publisherPage).subscribe({
+      next: page => this.publisherPage = page,
+    });
+    this.categoryService.getAll(this.categoryPage).subscribe({
+      next: page => this.categoryPage = page,
+    });
+    this.subcategoryService.getAll(this.subcategoryPage).subscribe({
+      next: page => this.subcategoryPage = page,
+    });
+  }
+
+  public emitFilter(): void {
+    this.filter.emit(this.form.value);
+  }
+}
