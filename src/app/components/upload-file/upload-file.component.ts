90dc648d69347923d3c9b603ad7b5019d7fdb81d
Anna Brito <annet.rj@gmail.com>
2024-05-21 22:50:45 -0300

    Add compoent to upload file

---
+++ src/app/components/upload-file/upload-file.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { v4 } from 'uuid';
+
+@Component({
+  selector: 'app-upload-file',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload-file.component.html',
+  styleUrl: './upload-file.component.scss'
+})
+export class UploadFileComponent {
+
+  public id: string = v4();
+
+  @Input()
+  public url: string | null = null;
+
+  @Output()
+  public selectFile: EventEmitter<File> = new EventEmitter();
+
+  public triggerFileInput(): void {
+    const input = document.getElementById(this.id) as HTMLInputElement;
+    input.click();
+  }
+
+  public getUrlImage(): string {
+    return this.url ?? "/assets/icons/image.png";
+  }
+
+  public onFileInputChange(target: any): void {
+    const files = target.files as FileList;
+    if(files.length > 0) {
+      this.url = URL.createObjectURL(files[0]);
+      this.selectFile.emit(files[0]);
+    }
+  }
+}
