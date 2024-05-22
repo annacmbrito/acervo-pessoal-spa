3e417e3e8b5aa30d55bd1e33124d22793c43a6f8
Anna Brito <annet.rj@gmail.com>
2024-05-21 22:50:04 -0300

    Add method to upload file

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import ImageKit from 'imagekit';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { v4 } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor() { }
+  private kit!: ImageKit;
+
+  constructor() {
+    this.kit = new ImageKit({
+      publicKey: environment.imageKit.publicKey,
+      privateKey: environment.imageKit.privateKey,
+      urlEndpoint: environment.imageKit.endpoint,
+    });
+  }
+
+  public upload(file: File): Observable<string> {
+    return new Observable(subscriber => {
+      this.readFile(file, (content) => {
+        this.kit.upload({
+          file: content,
+          fileName: v4(),
+        }, (err, result) => {
+          if(err) {
+            subscriber.error(err);
+          } else {
+            subscriber.next(result?.url);
+          }
+          subscriber.complete();
+        });
+      });
+    });
+  }
+
+  private readFile(file: File, callback: (content: string) => void) {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      callback((event.target!.result as string).split(',')[1]);
+    };
+    reader.readAsDataURL(file);
+  }
 }
