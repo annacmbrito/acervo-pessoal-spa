15f4dc683917951449755cd551b2c533cf0b1c6f
Anna Brito <annet.rj@gmail.com>
2024-05-22 01:35:39 -0300

    Add default book image link

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,6 @@
 export const environment = {
     apiBaseUrl: "<BASE_URL>",
+    bookImageDefault: 'https://ik.imagekit.io/qhzmow3ss/default.png?updatedAt=1716351623499',
     imageKit: {
         publicKey: '<IMAGE_KIT_PUBLIC_KEY>',
         privateKey : '<IMAGE_KIT_PUBLIC_KEY>',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     apiBaseUrl: "/api",
+    bookImageDefault: 'https://ik.imagekit.io/qhzmow3ss/default.png?updatedAt=1716351623499',
     imageKit: {
         publicKey: '<IMAGE_KIT_PUBLIC_KEY>',
         privateKey : '<IMAGE_KIT_PUBLIC_KEY>',
